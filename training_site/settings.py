3a7ea3ac7a5a137d22944829c2cc2874999e5ff0
SEnPRoger <dobchel3@gmail.com>
2023-02-08 20:56:48 +0200

    Update settings.py

--- training_site/settings.py
+++ training_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mpcp%0ne2rc#6&$punkqec#1#7d74lpv2pcxdum#vf=fxm3o^m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': [
