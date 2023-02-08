0091adcca9a7c4572a7ee94f1ba17ac0eb46cf51
SEnPRoger <dobchel3@gmail.com>
2023-02-08 20:50:55 +0200

    Update settings.py

--- training_site/settings.py
+++ training_site/settings.py
@@ -12,11 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os, sys
-import django_heroku, dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -25,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mpcp%0ne2rc#6&$punkqec#1#7d74lpv2pcxdum#vf=fxm3o^m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
