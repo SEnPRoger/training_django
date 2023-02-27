44edf495ce6f4d9764e188f496439386b14d817e
SEnPRoger <dobchel3@gmail.com>
2023-02-27 17:33:13 +0200

    Changed ALLOWED_HOSTS variable

--- auth_site/settings.py
+++ auth_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-d@n-k=owzos&%94a*^h_(3w07z9kr=bq52m%gda^^s#%e)#sw=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
