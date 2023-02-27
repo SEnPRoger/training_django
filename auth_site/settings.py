79850fc3a4f4150ad1c9cd25db906bd340e4a2e8
SEnPRoger <dobchel3@gmail.com>
2023-02-27 23:17:23 +0200

    Update settings.py

--- auth_site/settings.py
+++ auth_site/settings.py
@@ -163,6 +163,9 @@ JWTAUTH = {
     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
 }
 
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
