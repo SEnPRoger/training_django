c426407d3a514da5e84d5f20a5afd73edf698964
SEnPRoger <dobchel3@gmail.com>
2023-03-01 18:22:26 +0200

    Added samesite=None

--- JWTAuth/views.py
+++ JWTAuth/views.py
@@ -76,6 +76,7 @@ class JWTToken():
                             value = refresh_token,
                             expires = JWTToken.get_expires_date(refresh_token),
                             secure = True,
+                            samesite = 'None',
                             httponly = True)
 
     @staticmethod
