3204e20df21168315374218c17947c048f0006fc
SEnPRoger <dobchel3@gmail.com>
2023-03-03 11:33:17 +0200

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -23,4 +23,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/account/', include('account.urls', 'account_api')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
